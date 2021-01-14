d84f6876cddd036943136be794225afc164387ed
Journey Park <tenderain@gmail.com>
2021-01-14 18:20:09 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TOROCUS ERC20 smart contract
+
+open ganache and run truffle test in cli
+
+```bash
+yarn
+yarn test
+```
---
+++ migrations/2_deploy_torocus_token.js
@@ -0,0 +1,4 @@
+var TOROCUS = artifacts.require("./TOROCUS.sol");
+module.exports = function(deployer) {
+  deployer.deploy(TOROCUS);
+};
